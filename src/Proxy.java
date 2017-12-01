f1ac422e595b604ae9e7c27c99018a5e003c1d8a
António Matos <a.matos@campus.fct.unl.pt>
2017-12-01 07:29:24 +0000

    Escrevo a mensagem por aqui que já quero é ir domir...
    Ha um problema com os \pedidos http, por algum motivo depois de fazer o primeiro, ao fazer o segundo para ir buscar o init, dá erro... Acredito que seja alguma coisa em falta no cabeçalo, teste e verifica que se fizeres ao contrario (pedir primeiro o init),  este é recebido, mas na forma como agora esta tal nao acontece.
    
    Da uma vist ade olhos sff, perdi imenso tempo nisto, alem disso apercebime q para receber o EOF do txt, na versao 1.0 dava mal.
    
    Até amanha

--- src/Proxy.java
+++ src/Proxy.java
@@ -15,13 +15,14 @@ public class Proxy {
 	//*Primeira implementacao conta com unico tipo de segmentos pedidos (qld baixa)
 
 	private static final int CLIENTPORT = 1234;
-	private static final String SERVER_DEFAULT_URL = "http://192.168.99.100:8080";
+	private static final String SERVER_DEFAULT_URL = "http://localhost:8080";
 
 	//*Um so filme de cada vez
 	
 	private byte[] descriptor; 
 
 	public static void main(String[] args) throws IOException {
+		System.out.println("running");
 		String Serverurl = args.length == 1 ? args[0] : SERVER_DEFAULT_URL;
 		URL serverUrl = new URL(Serverurl);
 		InetAddress serverAddr = InetAddress.getByName(serverUrl.getHost());;
@@ -59,9 +60,9 @@ public class Proxy {
 		System.out.println("Descriptor Received");
 		//serverSocket.close();
 		
-		 //serverSocket = new Socket( serverAddr, serverPort );
+		 
 		 toServer.flush();
-		 //toServer = serverSocket.getOutputStream();
+		 
 		
 		//Obter Init File
 		serverRequest = "GET /"+ movieName + "/video/1/init.mp4 " + "HTTP/1.0 \r\n"
